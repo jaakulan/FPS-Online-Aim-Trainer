bcfd2bf1cffaf0ba17f0839deb19033c44ef38d4
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2021-08-16 19:00:55 -0400

    stuff

--- client/src/Components/MapPage/MapPage.jsx
+++ client/src/Components/MapPage/MapPage.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import styles from "./MapPage.module.css";
-import CatDust from "../../Assets/Maps/CSGO/CatDust.PNG";
-import CatMirage from "../../Assets/Maps/CSGO/CatMirage.PNG";
-import CrossingDust from "../../Assets/Maps/CSGO/CrossingDust.PNG";
-import AMirage from "../../Assets/Maps/CSGO/AMirage.PNG";
-import AMirage2 from "../../Assets/Maps/CSGO/AMirage2.PNG";
-import ATrain from "../../Assets/Maps/CSGO/ATrain.PNG";
-import BMirage from "../../Assets/Maps/CSGO/BMirage.PNG";
-import BMirage1 from "../../Assets/Maps/CSGO/BMirage1.PNG";
-import BDust from "../../Assets/Maps/CSGO/BDust.PNG";
-import MidTrain from "../../Assets/Maps/CSGO/MidTrain.PNG";
-import BTrain from "../../Assets/Maps/CSGO/BTrain.PNG";
+//CSGO MAPS========================================================
+import CatDust from "../../Assets/Maps/CSGO/CatDust.jpg";
+import CatMirage from "../../Assets/Maps/CSGO/CatMirage.jpg";
+import CrossingDust from "../../Assets/Maps/CSGO/CrossingDust.jpg";
+import AMirage from "../../Assets/Maps/CSGO/AMirage.jpg";
+import AMirage2 from "../../Assets/Maps/CSGO/AMirage2.jpg";
+import ATrain from "../../Assets/Maps/CSGO/ATrain.jpg";
+import BMirage from "../../Assets/Maps/CSGO/BMirage.jpg";
+import BMirage1 from "../../Assets/Maps/CSGO/BMirage1.jpg";
+import BDust from "../../Assets/Maps/CSGO/BDust.jpg";
+import MidTrain from "../../Assets/Maps/CSGO/MidTrain.jpg";
+import BTrain from "../../Assets/Maps/CSGO/BTrain.jpg";
 
 
 export default class MapPage extends Component {
@@ -28,14 +29,28 @@ export default class MapPage extends Component {
 
                 <div className={styles.row} >
                 <div className={styles.subtitle}>Cold War</div>
-                    <hr className= {styles.subtitle_line}/>
-                    <img src={CatDust} className={styles.map}/>
+                <hr className= {styles.subtitle_line}/>
+                    <div className={styles.mapImages}>
+                        <img src={CatDust} className={styles.map}/>
+                    </div>
                 </div>
 
                 <div className={styles.row}>
                 <div className={styles.subtitle}>CSGO</div>
-                    <hr className= {styles.subtitle_line}/>
-                    <img src={CatDust} className={styles.map}/>
+                <hr className= {styles.subtitle_line}/>
+                    <div className={styles.mapImages}>
+                        <img src={CatDust} className={styles.map}/>
+                        <img src={CatMirage} className={styles.map}/>
+                        <img src={CrossingDust} className={styles.map}/>
+                        <img src={AMirage} className={styles.map}/>
+                        <img src={AMirage2} className={styles.map}/>
+                        <img src={ATrain} className={styles.map}/>
+                        <img src={BMirage} className={styles.map}/>
+                        <img src={BMirage1} className={styles.map}/>
+                        <img src={BDust} className={styles.map}/>
+                        <img src={BTrain} className={styles.map}/>
+                        <img src={MidTrain} className={styles.map}/>
+                    </div>
                 </div>
 
                 <div className={styles.row}>
