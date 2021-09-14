32f524f9ca6607347a282a1d1db137c0077a561d
Parathan Vakeesan <parathan243@gmail.com>
2021-09-13 23:33:09 -0400

    Feat: Make Calculations for /analyze endpoint

--- server/server/API/views.py
+++ server/server/API/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import json
 
 # Create your views here.
 
 class AnalzyeView(APIView):
     def post(self, request):
-        return Response({'test': 'success'})
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        hit = body['hit']
+        missed = body['missed']
+        percentage = body['hit'] / (body['hit'] + body['missed'])
+        return Response({
+            'hit': hit,
+            'missed': missed,
+            'hitPercentage': percentage
+        })
 
