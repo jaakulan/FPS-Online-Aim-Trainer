29e8dbe67756584fe2d0bec1704c5c698989ba74
Parathan <parathan243@gmail.com>
2024-08-01 20:31:27 -0400

    init django tests

--- server/server/API/tests.py
+++ server/server/API/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+class Test(TestCase):
+
+    def test(self):
+        url = reverse('127.0.0.1:8000/analyze') 
+        data = {
+            "hit": 0.5,
+            "missed": 0.5
+        }
+
+        response = self.client.post(url, data, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
